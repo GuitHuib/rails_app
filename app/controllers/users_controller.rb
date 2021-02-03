671e3228e94d0efaef9c06f10e0f326849477ae4
= <=>
2021-02-03 10:06:55 -0600

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def index
-    @users = User.paginate( page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the site!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Check your email to activate your account."
+      redirect_to root_path
     else
       render 'new'
     end
