932bdf49af1b69f04ed72ff27bb511f690cceee9
= <=>
2021-02-07 14:59:38 -0600

    Add password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -8,13 +8,19 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["ryan@wheatonyamahamusic.com"], mail.from
-    assert_match user.name,             mail.body.encoded
-    assert_match user.activation_token, mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    assert_match user.name,              mail.body.encoded
+    assert_match user.activation_token,  mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
 
   test "password_reset" do
-    #stuff
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Reset password", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["ryan@wheatonyamahamusic.com"], mail.from
+    assert_match user.reset_token,       mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
-
 end
